f312eac9242661c2e3dbc08779f26dccf9bd8a68
sergeivrt <sergeivrt@gmail.com>
2024-02-05 18:59:14 +0200

    push

--- cypress/e2e/registration_form_4_test.cy.js
+++ cypress/e2e/registration_form_4_test.cy.js
@@ -9,7 +9,7 @@ describe('Input fields', () => {
 
         // in order to activate submit button, user has to click somewhere outside the input field
         cy.get('h2').contains('Password').click()
-        cy.get('#input_error_message').should('not.be.visible')
+        cy.get('#input_error_message').should('be.visible')
         cy.get('#success_message').should('not.be.visible')
     })
 
@@ -24,15 +24,15 @@ describe('Input fields', () => {
 
     it('Username should have min and max length values 1 and 50 characters', () => {
         // check that username element has min attribute value equalt to 1
-        cy.get('#username').should('have.attr', 'min', '11')
+        cy.get('#username').should('have.attr', 'min', '1')
 
         // check that username element has max attribute value equal to 50
-        cy.get('#username2').should('have.attr', 'max', '501')
+        cy.get('#username').should('have.attr', 'max', '50')
     })
 
     it('Username should support only letters and numbers', () => {
         // check with regex supporter format
-        cy.get('#username').should('have.attr', 'pattern', '[a-zA-Z0-9_]+')
+        cy.get('#username').should('have.attr', 'pattern', '[a-zA-Z0-9_]')
     })
 
     it('Email input should support correct pattern', () => {
